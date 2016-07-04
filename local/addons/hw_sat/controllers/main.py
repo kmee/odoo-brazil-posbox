0229544508ec0b7af685018e0ed7f76af9364ff3
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2016-07-04 12:44:00 -0300

    [ADD] Setado default_code do produto na funcao que cria o xml de envio para o Sefaz do CFe

--- local/addons/hw_sat/controllers/main.py
+++ local/addons/hw_sat/controllers/main.py
@@ -53,8 +53,8 @@ class Sat(Thread):
         self.lock = Lock()
         self.satlock = Lock()
         self.status = {'status': 'connecting', 'messages': []}
-        # self.printer = self._init_printer()
-        self.printer = False
+        self.printer = self._init_printer()
+        # self.printer = False
         self.device = self._get_device()
 
     def lockedstart(self):
@@ -119,7 +119,7 @@ class Sat(Thread):
     def __prepare_send_detail_cfe(self, item):
         return Detalhamento(
             produto=ProdutoServico(
-                cProd=unicode(int),
+                cProd=item['product_default_code'],
                 xProd=item['product_name'],
                 CFOP='5102',
                 uCom=item['unit_name'],
