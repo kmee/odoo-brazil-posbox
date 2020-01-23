1fbdabb407766c6537e90dbeb66d3f46f26fe4e9
Iván Todorovich <ivan.todorovich@gmail.com>
2020-01-23 13:03:44 +0100

    FIX install script

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+echo "Upgrading packages"
+sudo apt update && sudo apt upgrade -y
+
 echo "Installing docker.."
-sudo apt update && sudo apt upgrade
-sudo apt install -y docker docker-compose
+sudo apt install -y git docker docker-compose
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 sudo service docker restart
 sudo systemctl enable docker
 
 echo "Installing docker-posbox"
-sudo mkdir -p /posbox && chown -R $USER:$USER /posbox
+sudo mkdir -p /posbox
+sudo chown -R $USER:$USER /posbox
 git clone https://github.com/AwesomeFoodCoops/docker-posbox.git /posbox/docker-posbox
 cd /posbox/docker-posbox
 
 echo "Building image"
 sudo docker-compose build --pull
+sudo docker-compose start
