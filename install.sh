64981ec12e38b799fa3f3c8cd61c2992ab82e4a7
Iván Todorovich <ivan.todorovich@gmail.com>
2020-01-23 11:28:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo "Installing docker.."
+sudo apt update && sudo apt upgrade
 sudo apt install -y docker docker-compose
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 sudo service docker restart
 
+echo "Installing docker-posbox"
+sudo mkdir -p /posbox && chown -R $USER:$USER /posbox
+git clone https://github.com/AwesomeFoodCoops/docker-posbox.git /posbox/docker-posbox
+cd /posbox/docker-posbox
+
+echo "Building image"
+sudo docker-compose build --pull
